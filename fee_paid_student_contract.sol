876eba92f87b13a16a32e49b36732c2b40857211
Milad Raza <miladrazabhatti@gmail.com>
2022-09-11 11:31:53 +0500

    minor issue fixed

--- fee_paid_student_contract.sol
+++ fee_paid_student_contract.sol
@@ -12,7 +12,7 @@ contract CoachingCenter{
     mapping(address => student) feePaidStidents;
 
     function payFee(string memory name) public payable returns(string memory){
-        require(msg.value >= 1 ether, "Fee is 1 ether");
+        require(msg.value != 1 ether, "Fee is 1 ether");
         feePaidStidents[msg.sender] = student(name, true, msg.sender, block.timestamp);
         return "Fees Paid Successfully";
     }
